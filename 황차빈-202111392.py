9fe4c0fc479937ca3b41233896951501289b1816
chabin37 <7chabin@naver.com>
2023-09-24 15:56:59 +0900

    fin

--- 황차빈-202111392.py
+++ 황차빈-202111392.py
@@ -30,19 +30,23 @@ for i in range(K):
 
 
 JClust = np.array([])
-for i in range(iteration_n):
+for i in range(iteration_n):  # JClust라는 배열을 선언
     distance = np.array([])
     # 무작위로 설정된 각 집합의 중심점과 다른 모든 점과의 거리 구하기
-    for j in range(K):  # j = 0~K-1
-        for k in range(title_n):  # k = 0~499
+    for j in range(K):  # j = 0~K-1 16번
+        for k in range(title_n):  # k = 0~499 500번
             dis_sum = 0
             for l in range(word_n):  # l = 0~4422
                 dis_sum = dis_sum + ((D[k, l]-rep[j, l])**2)
-            temp_dis = math.sqrt(dis_sum)
+            temp_dis = math.sqrt(dis_sum)  # 제곱근 반환
             distance = np.append(distance, temp_dis)
+            # distance에 temp_dis를 추가하고, distance배열에 반환 500*16번<이러면 8천번이여야하는데
     JClust = np.append(JClust, distance.sum()/title_n)
+    # JClust배열에 distance값들의 평균을 배열에 추가함
     distance = distance.reshape(K, 500)
+    # 'K행 500열'로 distance 배열을 재정의함
     C = np.argmin(distance, axis=0)+1
+    # 인덱스값+1을 반환해서 배열C에 저장, axis가 0이면 행을 따라 연산, 1이면 열을 따라 연산
     Y = {}
     for m in range(K):
         Y[m+1] = np.empty((0, word_n), float)
@@ -58,7 +62,7 @@ for i in range(iteration_n):
                 t = t/Y[o+1].shape[0]
             ddd = np.append(ddd, t)
     rep = np.empty((0, word_n), float)
-    rep = ddd.reshape(4423, K).T
+    rep = ddd.reshape(4422, K).T
 
 
 iter = np.array([])
