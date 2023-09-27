a482b322841897a20d81281f0618ef1ca986fae1
chabin37 <7chabin@naver.com>
2023-09-27 22:24:09 +0900

    1

--- 황차빈-202111392.py
+++ 황차빈-202111392.py
@@ -1,71 +1,33 @@
 # 선형대수학 K Mean 클러스터링
-
-
-import math
+import math as mt
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import random as rd
 
-data = pd.read_excel('termDocMatrix.xlsx')
-data.head()
+data = pd.read_excel("termDocMatrix.xlsx", engine="openpyxl", header=None)
+K = 9
+# K는 임의의 중심 갯수
+arr = np.array(data)
+row = len(arr)
+col = len(arr[1])
+# A열이 nan값이므로, col-1이 총 열수라고 볼 수 있음
+arr2 = np.transpose(arr)
 
+print(row, "row 4423임", col, "col 501", arr[3][5])
 
-D = data.iloc[:, 0:500].values
-D = data.values.T
-word_n = D.shape[1]  # 4423
-title_n = D.shape[0]  # 500
-
-
-K = 16  # 집합 수
-iteration_n = 20  # 클러스터링
-rep = np.empty((0, word_n), float)  # 각 집합의 중심점
-
-
-# 각 집합의 중심점 무작위로 설정
-for i in range(K):
-    rand = rd.randint(0, title_n-1)
-    rep = np.append(rep, [D[rand]], axis=0)
-
-
-JClust = np.array([])
-for i in range(iteration_n):
-    distance = np.array([])
-    # 무작위로 설정된 각 집합의 중심점과 다른 모든 점과의 거리 구하기
-    for j in range(K):  # j = 0~K-1
-        for k in range(title_n):  # k = 0~499
-            dis_sum = 0
-            for l in range(word_n):  # l = 0~4422
-                dis_sum = dis_sum + ((D[k, l]-rep[j, l])**2)
-            temp_dis = math.sqrt(dis_sum)
-            distance = np.append(distance, temp_dis)
-    JClust = np.append(JClust, distance.sum()/title_n)
-    distance = distance.reshape(K, 500)
-    C = np.argmin(distance, axis=0)+1
-    Y = {}
-    for m in range(K):
-        Y[m+1] = np.empty((0, word_n), float)
-    for n in range(title_n):
-        Y[C[n]] = np.append(Y[C[n]], [D[n]], axis=0)
-        ddd = np.array([])
-    for o in range(K):
-        for p in range(word_n):
-            t = 0
-            for q in range(Y[o+1].shape[0]):
-                t = t+Y[o+1][q][p]
-            if (Y[o+1].shape[0] != 0):
-                t = t/Y[o+1].shape[0]
-            ddd = np.append(ddd, t)
-    rep = np.empty((0, word_n), float)
-    rep = ddd.reshape(4423, K).T
-
-
-iter = np.array([])
-for i in range(iteration_n):
-    iter = np.append(iter, i+1)
-plt.plot(iter, JClust)
-plt.xlabel('iteration')
-plt.ylabel('JClust')
-plt.xlim(0, 22)
-plt.show()
+random_k = rd.sample(range(1, col), K)
+# 9개 중심점의 문서를 정함
+# 각 중심점과 길이를 비교
+list = np.empty((K, col-1))  # 9,500배열
+# list는 중심과 그 중심 그룹으로 묶인 문서를 저장하는 리스트이다
+# list_temp는 최솟값을 구하기 위한 임시 배열
+# 이 아래는 클러스터링 행위
+for k in range(col-1):  # 소문자k대문자K 헷갈리는거주의
+    list_temp = np.empty(K)  # K개 크기의 배열
+    for i in range(K):
+        dist = np.linalg.norm(arr2[k+1]-arr2[random_k[i]])
+        list_temp[i] = dist  # 백터의 거리를list_temp에 저장
+    min = np.argmin(list_temp)
+    list[min].appand(k)  # min그룹에k문서넣기
+print(dist)
