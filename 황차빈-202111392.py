9e77c1a67e9eaa1026f315515f57fa565216d30b
chabin37 <7chabin@naver.com>
2023-09-27 22:52:56 +0900

    1

--- 황차빈-202111392.py
+++ 황차빈-202111392.py
@@ -4,3 +4,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
+random_k = rd.sample(range(1, col), K)
+# 9개 중심점의 문서를 정함
+# 각 중심점과 길이를 비교
+list = np.empty((K, col-1))  # 9,500배열
+# list는 중심과 그 중심 그룹으로 묶인 문서를 저장하는 리스트이다
+# list_temp는 최솟값을 구하기 위한 임시 배열
+# 이 아래는 클러스터링 행위
+for k in range(col-1):  # 소문자k대문자K 헷갈리는거주의
+    list_temp = np.empty(K)  # K개 크기의 배열
+    for i in range(K):
+        dist = np.linalg.norm(arr2[k+1]-arr2[random_k[i]])
+        list_temp[i] = dist  # 백터의 거리를list_temp에 저장
+    min = np.argmin(list_temp)
+    list[min].appand(k)  # min그룹에k문서넣기
+print(dist)
