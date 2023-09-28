89b6834795b3d1794f6bb63497a837838d61edd5
chabin37 <7chabin@naver.com>
2023-09-28 22:26:26 +0900

    1

--- 황차빈-202111392.py
+++ 황차빈-202111392.py
@@ -4,32 +4,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
+from collections import defaultdict
 
 data = pd.read_excel("termDocMatrix.xlsx", engine="openpyxl", header=None)
-K = 9
-# K는 임의의 중심 갯수
-arr = np.array(data)
+K = 9                       # K는 임의의 중심 갯수
+arr = np.array(data)        # arr는 data배열을 numpy배열로 바꿈
 row = len(arr)
 col = len(arr[1])
 
-# A열이 nan값이므로, col-1이 총 열수라고 볼 수 있음
-arr2 = np.transpose(arr)
-# 문서끼리 비교해야 하므로, 한 열의 값을 한번에 불러올 수 없음. 따라서 행과 열을 전치
-# arr2의 각 행은 문서에 해당하고, 각 열들은 단어에 해당함.
-print(row, "row 4423임", col, "col 501", arr[3][5])
-# 테스트 문장
-random_k = rd.sample(range(1, col), K)
-# 1~501사이, K개의 중심점의 좌표를 정함.
-# 각 중심점과 길이를 비교
+def vector_distance_calculate(vector1,vector2):     #두 백터의 거리를 계산하는 함수
+    return np.linalg.norm(vector2-vector1)          # A열이 nan값이므로, col-1이 총 열수라고 볼 수 있음
+
+arr2 = np.transpose(arr)                # 문서끼리 비교해야 하므로, 한 열의 값을 한번에 불러올 수 없음. 따라서 행과 열을 전치
+random_k = rd.sample(range(1, col), K)  # 1~501사이, K개의 중심점의 좌표를 정함.각 중심점과 길이를 비교
+dictionary= defaultdict(list)           #중심과 그 집합을 모은 딕셔너리 생성
+k = len(random_k)                       # 중심의 갯수
+
+for m in range(len(arr2)-1):                          #500개의 문서를 9개의 중심들과 거리 비교 후, 가장 짧은 곳 키인덱스:문서인덱스 딕셔너리에 넣음
+        dist=[vector_distance_calculate(arr2[i]-arr2[m+1])for i in range(K)]
+        dictionary[np.argmin(dist)].append(m+1)       # 가장 작은 값의 인덱스(0~8중하나), m은 문서번호(배열상의)를 의미함
+
+#위 for문이 끝나면, 딕셔너리는 4:[2,3,23,413 ....와 같이 분류됨.
+#가상의 중심 만들어야 함. 
+arr_tool=dictionary.get[4]
 
 
-def k_means_klustering(data, center_arr, repeat):
-    k = len(center_arr)  # 중심의 갯수
-    # center_arr[0]<문서 인덱스
-    # data[center_arr]<문서 벡터
-    for m in range(len(data)):  # 여기서는 m번문서와, 중심문서k 9개를 비교함. 이후 배열함(첫행이 비어서 m+1)
-        dist = (np.linalg.norm(data[center_arr[i]]-data[m+1])for i in range(K))
 
+
+group_list.append(np.argmin(dist))
+
+
+def k_means_klustering(data, center_arr):
 
 dist = np.linalg.norm(arr2[k+1]-arr2[random_k[i]])
 # 각 벡터와 기준과 백터거리계산. 원소(두 백터의 차)의 곱의 합의 제곱근.
+
+#J-clust=>그룹의 중심-그룹의 원소 거리norm한것들의 총합.
