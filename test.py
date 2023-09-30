7e5535ceb483e194fa1fcdc713ca49f9a86d8135
chabin37 <7chabin@naver.com>
2023-09-30 12:25:56 +0900

    11

--- test.py
+++ test.py
@@ -1,15 +1,31 @@
 import numpy as np
 
+data_arr = [
+    {1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {
+        1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {1, 2, 3, 4, 5}
+]
 
 dictionary = {
-    "a": [1, 2, 3],
-    "b": [4, 5, 6]
+    "1": [1, 2, 3],
+    "2": [4, 5, 6]
 }
 
 
+def Calculate_Jclust(dictionary, data_arr,  K):  # J클러스트값 계산
+    clust = 0
+    for m in range(K):
+        K_dictionary_value = dictionary.get(m)
+        for i in range(len(K_dictionary_value)):
+            clust = clust + \
+                vector_distance_calculate(
+                    data_arr[m], data_arr[K_dictionary_value[i]])
+    return clust
+
+
 def vector_distance_calculate(vector1, vector2):
-    # 두 백터의 거리를 계산하는 함수
     return np.linalg.norm(np.array(vector2)-np.array(vector1))
 
 
 print(vector_distance_calculate(dictionary.get("a"), dictionary.get("b")))
+print(dictionary.get("a"))
+print(Calculate_Jclust(dictionary, data_arr, 2))
